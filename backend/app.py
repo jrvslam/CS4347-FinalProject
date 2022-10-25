5a2da8cb129a225f1b49eb2367b2a14d5e114683
anand-subu <anand.subu10@gmail.com>
2022-10-26 02:24:21 +0800

    Add route for transcribing lyrics. Add WSGI server bindings for app through gevent

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 import torch
+import tempfile
+from gevent.pywsgi import WSGIServer
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -9,7 +11,7 @@ sys.path.append("..")
 
 from melody_extraction.main import AST_Model
 from melody_extraction.inference import predict_one_song
-
+from speechbrain.pretrained import EncoderDecoderASR
 from flask import Flask, jsonify, request, flash, redirect
 
 app = Flask(__name__)
@@ -21,8 +23,15 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 best_model = AST_Model(device, MELODY_MODEL_PATH)
 
+asr_model = EncoderDecoderASR.from_hparams(
+    source="trained_model",
+    hparams_file="hyperparams.yaml",
+    savedir="pretrained_model"
+)
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'mp3'
+
 
 @app.route('/melody', methods=['GET', 'POST'])
 def predict_melody():
@@ -37,8 +46,8 @@ def predict_melody():
         if file and allowed_file(file.filename):
             file.save(MELODY_FILE_PATH)
             results = {}
-            results = predict_one_song(best_model, MELODY_FILE_PATH, 'result', results, tomidi=False, output_path='', 
-                            onset_thres=0.4, offset_thres=0.5)
+            results = predict_one_song(best_model, MELODY_FILE_PATH, 'result', results, tomidi=False, output_path='',
+                                       onset_thres=0.4, offset_thres=0.5)
             return jsonify(results)
     return '''
         <html>
@@ -51,3 +60,39 @@ def predict_melody():
             </body>
         </html>
     '''
+
+
+@app.route('/lyrics', methods=['GET', 'POST'])
+def transcribe_lyrics():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No selected file')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            handle, filename = tempfile.mkstemp()
+            file.save(filename)
+            transcription = asr_model.transcribe_file(filename)
+            file.close()
+            payload = {"text": transcription}
+            return jsonify(payload)
+    return '''
+        <html>
+            <body>
+                <form action = "http://localhost:5000/lyrics" method = "POST" 
+                    enctype = "multipart/form-data">
+                    <input type = "file" name = "file" />
+                    <input type = "submit"/>
+                </form>   
+            </body>
+        </html>
+    '''
+
+
+if __name__ == '__main__':
+    app.secret_key = 'hTWWG8UKwqt-PjWNkkAwkA'
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
