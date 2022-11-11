70326ebe67a0e09ae7f7b3cc938f0bc86fcaa7ae
arindamshivatrip <arindamtripathi@gmail.com>
2022-11-11 18:17:24 +0800

    piano roll works

--- frontend/src/ResultsSection.js
+++ frontend/src/ResultsSection.js
@@ -27,15 +27,8 @@ export function ResultsSection(melody_output, lyrics_output) {
                       gridLineColor={0x333333}
                       blackGridBgColor={0x1e1e1e}
                       whiteGridBgColor={0x282828}
-                      noteData={[
-                        ["0:0:0", "F5", ""],
-                        ["0:0:0", "C4", "2n"],
-                        ["0:0:0", "D4", "2n"],
-                        ["0:0:0", "E4", "2n"],
-                        ["0:2:0", "B4", "4n"],
-                        ["0:3:0", "A#4", "4n"],
-                        ["0:0:0", "F2", ""],
-                      ]}
+                      noteFormat={"MIDI"}
+                      noteData={melody_output}
                     />
                   </div>
         }
