40d014d21add55ca9b267e0d986d6065fd8b14d1
zhaojj2209 <zhaojj2000@gmail.com>
2022-11-11 16:47:28 +0800

    Add piano roll

--- frontend/src/ResultsSection.js
+++ frontend/src/ResultsSection.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { setProcessState, setMelodyRes, setLyricRes } from "./features/configureSlice";
-// import PianoRoll from "react-piano-roll";
+import PianoRoll from "./components/PianoRoll";
 
 export function ResultsSection(melody_output, lyrics_output) {
     const configuration = useSelector((state) => state.configuration);
@@ -18,17 +18,25 @@ export function ResultsSection(melody_output, lyrics_output) {
                     
                     <h2>Lyrics Output: </h2>
                     <h3>{lyrics_output.text}</h3>
-                    {/* <PianoRoll
+                    <PianoRoll
                       bpm={150}
-                      width={1200}
-                      height={660}
+                      width={500}
+                      height={350}
                       zoom={6}
                       resolution={2}
                       gridLineColor={0x333333}
                       blackGridBgColor={0x1e1e1e}
                       whiteGridBgColor={0x282828}
-                      noteData={melody_output}
-                    /> */}
+                      noteData={[
+                        ["0:0:0", "F5", ""],
+                        ["0:0:0", "C4", "2n"],
+                        ["0:0:0", "D4", "2n"],
+                        ["0:0:0", "E4", "2n"],
+                        ["0:2:0", "B4", "4n"],
+                        ["0:3:0", "A#4", "4n"],
+                        ["0:0:0", "F2", ""],
+                      ]}
+                    />
                   </div>
         }
     </div>
