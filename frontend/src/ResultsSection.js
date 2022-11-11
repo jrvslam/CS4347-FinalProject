d6fdd0c409c7fa203729eefb49da8607fdcf8768
zhaojj2209 <zhaojj2000@gmail.com>
2022-11-11 15:40:06 +0800

    Fallback chart

--- frontend/src/ResultsSection.js
+++ frontend/src/ResultsSection.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from 'react-redux'
+import Plot from 'react-plotly.js';
 
 export function ResultsSection() {
-  const configuration = useSelector((state) => state.configuration);
+  const processState = useSelector((state) => state.configuration.processState);
+  const melodyRes = useSelector((state) => state.configuration.melodyRes);
+  const lyricRes = useSelector((state) => state.configuration.lyricRes);
+  const [x, setX] = useState([]);
+  const [y, setY] = useState([]);
+
+  useEffect(() => {
+    const newX = [];
+    const newY = [];
+    melodyRes.forEach((note) => {
+      console.log(note);
+      newX.push(note[0]);
+      newX.push(note[1]);
+      newY.push(note[2]);
+      newY.push(note[2]);
+    })
+    setX(newX);
+    setY(newY);
+  }, [melodyRes]);
 
   return (
     <div>
         <h2>Results</h2>
         {
-            (configuration.processState !== 2) 
+            (processState !== 2) 
                 ? <p> Start Analysing some music!</p> 
-                : <p> Lyrics: {configuration.lyricRes}</p>
+                : <div>
+                  <Plot
+                    data={[
+                      {
+                        x,
+                        y,
+                        type: 'scatter',
+                      },
+                    ]}
+                    layout={{width: 500, height: 350, title: 'Melody'}}
+                  />
+                  <p> Lyrics: {lyricRes}</p>
+                  </div>
         }
     </div>
   );
